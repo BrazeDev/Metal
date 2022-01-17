95e4318509b3551c158e6e62facd52bbbca56aaf
Owen Flaherty <thatsimplekid@gmail.com>
2022-01-17 10:43:24 +0000

    docs: fixed shield script

--- scripts/createshield.js
+++ scripts/createshield.js
@@ -5,7 +5,8 @@ const result = require('../mochawesome-report/mochawesome.json')
 const output = {
     schemaVersion: 1,
     label: 'tests',
-    message: `${result.stats.passes} passed, ${result.stats.failures} failed`
+    message: `${result.stats.passes} passed, ${result.stats.failures} failed`,
+    color: result.stats.failures > 0 ? 'red' : 'green'
 }
 
 fs.writeFileSync(path.join(__dirname, '..', 'mochawesome-report', 'shield.json'), JSON.stringify(output))
